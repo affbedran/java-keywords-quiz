84a2da6e94a6962b26054d92689e11c7870e275d
André Bedran <affbedran@gmail.com>
2019-08-25 13:08:39 -0300

    Defined a protocol to represent viewModel. Implemented background fetch.

--- javaquiz/javaquiz/ViewModel/QuizViewModel.swift
+++ javaquiz/javaquiz/ViewModel/QuizViewModel.swift
@@ -13,13 +13,50 @@ protocol QuizDelegate {
     func didHitAnswer()
     func didRunOutOfTime()
     func didFinishQuiz()
+    func didFinishLoading()
 }
 
-class QuizViewModel {
+protocol QuizViewModelType {
+    var path: Path {get}
+    var hits: [String] {get}
+    var answer: [String] {get}
+    var question: String? {get}
+    var delegate: QuizDelegate? {get set}
+    func getQuiz(for path: Path)
+    func textForHit(at path: IndexPath) -> String?
+    
+}
+
+class QuizViewModel: QuizViewModelType {
+
+    private(set) var path: Path = .java
     
     private(set) var hits: [String] = []
     private(set) var answer: [String] = []
+    private(set) var question: String?
+    
     var delegate: QuizDelegate?
+    
+    init(path: Path = .java) {
+        self.path = path
+    }
+    
+    func getQuiz(for path: Path) {
+        NetworkManager.shared.requestQuiz(at: path) { (data, error) in
+            do {
+                guard let data = data else { return }
+                let quiz = try JSONDecoder().decode(Quiz.self, from: data)
+                if let question = quiz.question, let answer = quiz.answer {
+                    self.answer = answer
+                    self.question = question
+                    self.delegate?.didFinishLoading()
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
     
     func textForHit(at path: IndexPath) -> String? {
         let row = path.row
